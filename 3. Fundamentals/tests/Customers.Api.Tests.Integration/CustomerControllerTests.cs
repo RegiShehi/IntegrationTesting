0067dc969e4f81e73668a74bb7b13fabea48a9c0
Regi Shehi <regi_shehi@hotmail.com>
2024-12-27 09:35:55 +0100

    Initial commit

---
+++ 3. Fundamentals/tests/Customers.Api.Tests.Integration/CustomerControllerTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Customers.Api.Tests.Integration;
+
+public class CustomerControllerTests
+{
+    private readonly HttpClient _httpClient = new()
+    {
+        BaseAddress = new Uri("https://localhost:5001")
+    };
+
+    private readonly WebApplicationFactory<IApiMarker> _webApplicationFactory = new();
+
+    [Fact]
+    public async Task Get_ReturnsNotFound_WhenCustomerDoesNotExist()
+    {
+        // Arrange
+
+        // Act
+        var response = await _httpClient.GetAsync($"customers/{Guid.NewGuid()}");
+
+        // Assert
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+}
