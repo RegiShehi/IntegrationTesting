a7d5eae7d7143c8d7a175ed4961dbd476ace3c7f
Regi Shehi <regi_shehi@hotmail.com>
2024-12-29 21:11:37 +0100

    Add first integration test

--- 3. Fundamentals/tests/Customers.Api.Tests.Integration/CustomerApiFactory.cs
+++ 3. Fundamentals/tests/Customers.Api.Tests.Integration/CustomerApiFactory.cs
@@ -5,18 +5,23 @@ using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
 using Testcontainers.PostgreSql;
 
 namespace Customers.Api.Tests.Integration;
 
 public class CustomerApiFactory : WebApplicationFactory<IApiMarker>, IAsyncLifetime
 {
+    public const string ValidGitHubUsername = "validuser";
+
     private readonly PostgreSqlContainer _dbContainer =
         new PostgreSqlBuilder()
             .WithDatabase("mydb")
             .WithUsername("workshop")
             .WithPassword("changeme")
             .Build();
+
+    private readonly GitHubApiServer _gitHubApiServer = new();
 
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
@@ -28,16 +33,28 @@ public class CustomerApiFactory : WebApplicationFactory<IApiMarker>, IAsyncLifet
 
             services.AddSingleton<IDbConnectionFactory>(_ =>
                 new NpgsqlConnectionFactory(_dbContainer.GetConnectionString()));
+
+            services.AddHttpClient("GitHub", httpClient =>
+            {
+                httpClient.BaseAddress = new Uri(_gitHubApiServer.Url);
+                httpClient.DefaultRequestHeaders.Add(
+                    HeaderNames.Accept, "application/vnd.github.v3+json");
+                httpClient.DefaultRequestHeaders.Add(
+                    HeaderNames.UserAgent, $"Course-{Environment.MachineName}");
+            });
         });
     }
 
     public async Task InitializeAsync()
     {
+        _gitHubApiServer.Start();
+        _gitHubApiServer.SetupUser(ValidGitHubUsername);
         await _dbContainer.StartAsync();
     }
 
     public new async Task DisposeAsync()
     {
         await _dbContainer.DisposeAsync();
+        _gitHubApiServer.Dispose();
     }
 }
--- 3. Fundamentals/tests/Customers.Api.Tests.Integration/CustomerController/CreateCustomerControllerTests.cs
+++ 3. Fundamentals/tests/Customers.Api.Tests.Integration/CustomerController/CreateCustomerControllerTests.cs
@@ -1,18 +1,43 @@
-﻿namespace Customers.Api.Tests.Integration.CustomerController;
+﻿using System.Net;
+using System.Net.Http.Json;
+using Bogus;
+using Customers.Api.Contracts.Requests;
+using Customers.Api.Contracts.Responses;
+using FluentAssertions;
+
+namespace Customers.Api.Tests.Integration.CustomerController;
 
 // [Collection("CustomerApi Collection")]
 public class CreateCustomerControllerTests : IClassFixture<CustomerApiFactory>
 {
     private readonly CustomerApiFactory _apiFactory;
+    private readonly HttpClient _client;
+
+    private readonly Faker<CustomerRequest> _customerGenerator = new Faker<CustomerRequest>()
+        .RuleFor(x => x.Email, faker => faker.Person.Email)
+        .RuleFor(x => x.FullName, faker => faker.Person.FullName)
+        .RuleFor(x => x.GitHubUsername, CustomerApiFactory.ValidGitHubUsername)
+        .RuleFor(x => x.DateOfBirth, faker => faker.Person.DateOfBirth.Date);
 
     public CreateCustomerControllerTests(CustomerApiFactory apiFactory)
     {
         _apiFactory = apiFactory;
+        _client = apiFactory.CreateClient();
     }
 
     [Fact]
-    public async Task Test()
+    public async Task Create_CreatesUser_WhenDataIsValid()
     {
-        await Task.Delay(2000);
+        // arrange
+        var customer = _customerGenerator.Generate();
+
+        // act
+        var response = await _client.PostAsJsonAsync("customers", customer);
+
+        // assert
+        var customerResponse = await response.Content.ReadFromJsonAsync<CustomerResponse>();
+        customerResponse.Should().BeEquivalentTo(customer);
+        response.StatusCode.Should().Be(HttpStatusCode.Created);
+        response.Headers.Location!.ToString().Should().Be($"http://localhost/customers/{customerResponse!.Id}");
     }
 }
